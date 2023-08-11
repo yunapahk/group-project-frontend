e2f9420843cebb01569f68ff5f9ec4c39dc86678
Yuna Pahk <new1@new1s-MacBook-Pro.local>
2023-08-11 14:20:53 -0700

    Update card updated

--- frontend/src/pages/Show.js
+++ frontend/src/pages/Show.js
@@ -5,22 +5,21 @@ function Show(props) {
   console.log(note);
 
   return (
-    <div className="note">
+    <div className="notes">
       <h2>{note.title}</h2>
       <h2>{note.description}</h2>
       <h2>{note.url}</h2>
 
-    <h2>Update {note.title}</h2>
+    <h2 className="note-container">Update {note.title}</h2>
     <Form action={`/update/${note._id}`} method="post">
         <input type="text" name="title" placeholder="Note title" defaultValue={note.title}/>
         <input type="text" name="description" placeholder="description" defaultValue={note.description}/>
         <input type="text" name="url" placeholder="url" defaultValue={note.url}/>
-        <input type="submit" value="Update Note"/>
+        <input type="submit" value="Done"/>
     </Form>
 
-    <h2>Delete Note</h2>
       <Form action={`/delete/${note._id}`} method="post">
-        <input type="submit" value="Delete Note"/>
+        <input type="submit" value="Delete"/>
       </Form>
     </div>
   );
